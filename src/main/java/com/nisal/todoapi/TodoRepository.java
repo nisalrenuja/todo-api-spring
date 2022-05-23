e2c01de61e85dfb5c19104bded1eed3583dcb647
nisalrenuja <nrenuja@gmail.com>
2022-05-24 00:44:01 +0530

    initial commit

---
+++ src/main/java/com/nisal/todoapi/TodoRepository.java
@@ -0,0 +1,7 @@
+package com.nisal.todoapi;
+
+import com.nisal.todoapi.entities.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+}
---
+++ src/main/java/com/nisal/todoapi/entities/Todo.java
@@ -0,0 +1,27 @@
+package com.nisal.todoapi.entities;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@RequiredArgsConstructor
+@Table(name = "todo")
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "completed")
+    private Boolean completed;
+
+}
